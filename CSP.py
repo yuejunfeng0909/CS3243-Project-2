4c0abac2182b53b2f47bb5b8630b5604a0622193
Yue Junfeng <yuejunfeng0909@gmail.com>
2022-03-18 20:31:35 +0800

    some optimization
    remove .key() for iteration
    optimization?
    
    
    some more improvements?

--- CSP.py
+++ CSP.py
@@ -1,5 +1,4 @@
 import sys
-# from time import time
 import itertools
 # import numpy as np
 
@@ -35,7 +34,7 @@ class Board:
             "Rook": 0, 
             "Knight": 0
         } 
-        self.obstaclePos = set() # to keep track which position to skip
+        self.obstaclePos = {} # to keep track which position to skip
 
         self.board_size_x = cols
         self.board_size_y = rows
@@ -43,12 +42,12 @@ class Board:
         
         # 2D array to store possible enemy types at the particular position
         self.possibleEnemyTypes = []
-        sample = set()
+        sample = {}
 
         # only add the pieces that we are interested in into the sample
         for enemy in Piece.enemyTypes:
             if maxNumOfEachEnemy[enemy] != 0:
-                sample.add(enemy)
+                sample[enemy] = True
 
         for i in range(cols):
             self.possibleEnemyTypes.append([])
@@ -61,32 +60,40 @@ class Board:
 
     def isBlocked(self, x: int, y:int) -> bool:
         '''Occupied by enemy piece or by obstacle piece'''
-        return ((x, y) in self.obstaclePos) or ((x, y) in self.enemyPos.keys())
+        return ((x, y) in self.obstaclePos) or ((x, y) in self.enemyPos)
 
     def isOccupiedByEnemyPiece(self, x: int, y: int) -> bool:
-        return (x, y) in self.enemyPos.keys()
+        return (x, y) in self.enemyPos
 
     def addEnemyPiece(self, pieceType: str, x: int, y: int) -> None:
         self.enemyPos[(x, y)] = pieceType
-        self.possibleEnemyTypes[x][y] = {pieceType}
+        self.possibleEnemyTypes[x][y] = {pieceType: True}
         self.numberOfEachEnemy[pieceType] += 1
         
         # mark all threatened position's available pieces as []
         transitionModel = pieceMovementModel(self, x, y, pieceType)
-        for threatenedX, threatenedY in transitionModel.getAllPossibleNewPos():
-            self.possibleEnemyTypes[threatenedX][threatenedY] = set()
+        # for threatenedX, threatenedY in transitionModel.getAllPossibleNewPos():
+        #     self.possibleEnemyTypes[threatenedX][threatenedY] = set()
+        def f(i, j):
+            self.possibleEnemyTypes[i][j] = {}
+        transitionModel.getAllPossibleNewPos(f)
+
 
         # for all other pieces type, T, at the position, mark the threatened available pieces -T
         for otherType in Piece.enemyTypes:
             if otherType == pieceType:
                 continue
             transitionModel = pieceMovementModel(self, x, y, otherType)
-            for threatenedX, threatenedY in transitionModel.getAllPossibleNewPos():
-                self.possibleEnemyTypes[threatenedX][threatenedY].discard(otherType)
+            # for threatenedX, threatenedY in transitionModel.getAllPossibleNewPos():
+            #     self.possibleEnemyTypes[threatenedX][threatenedY].discard(otherType)
+            def f(i, j):
+                if otherType in self.possibleEnemyTypes[i][j]:
+                    del self.possibleEnemyTypes[i][j][otherType]
+            transitionModel.getAllPossibleNewPos(f)
         
     def addObstaclePiece(self, x: int, y: int) -> None:
-        self.obstaclePos.add((x, y))
-        self.possibleEnemyTypes[x][y] = set()
+        self.obstaclePos[(x, y)] = True
+        self.possibleEnemyTypes[x][y] = {}
 
     def isWithinBoard(self, x, y) -> bool:
         if (0 > x or x >= self.board_size_x) or (0 > y or y >= self.board_size_y):
@@ -103,33 +110,91 @@ class pieceMovementModel():
         self.movements = Piece.movement[pieceType]
 
     def moveToDirection(self, x_change: int, y_change: int):
-        new_x = self.x + x_change
-        new_y = self.y + y_change
-        return (new_x, new_y)
+        return (self.x + x_change, self.y + y_change)
 
-    def __getAllPossibleMovementToDirection(self, x_change: int, y_change: int, max_steps=0):
+    def __getAllPossibleMovementToDirection(self, x_change: int, y_change: int, max_steps=0, f = None):
         '''Get all the positions that the piece can move to, including position that are being threatened by other pieces'''
         if max_steps == 0:
             max_steps = max(self.board.board_size_x, self.board.board_size_y)
-        steps = []
+        
+        if f == None:
+            steps = []
+            for i in range(max_steps):
+                new_pos = self.moveToDirection((i+1) * x_change, (i+1) * y_change)
+                if (not self.board.isWithinBoard(new_pos[0], new_pos[1])):
+                    break
+                if self.board.isBlocked(new_pos[0], new_pos[1]):
+                    # in the context of CSP, the piece violates constraints
+                    # print("ERROR: CONSTRAINT VIOLATION", new_pos[0], new_pos[1])
+                    steps.append(new_pos)
+                    break
+                steps.append(new_pos)
+            return steps
+        else:
+            for i in range(max_steps):
+                new_pos = self.moveToDirection((i+1) * x_change, (i+1) * y_change)
+                if (not self.board.isWithinBoard(new_pos[0], new_pos[1])):
+                    break
+                if self.board.isBlocked(new_pos[0], new_pos[1]):
+                    f(new_pos[0], new_pos[1])
+                    break
+                f(new_pos[0], new_pos[1])
+
+    def getAllPossibleNewPos(self, f = None):
+        if f == None :
+            steps = []
+            for movement in self.movements:
+                xChange, yChange, maxSteps = movement
+                steps.extend(self.__getAllPossibleMovementToDirection(xChange, yChange, maxSteps))
+            return steps
+        else:
+            for movement in self.movements:
+                xChange, yChange, maxSteps = movement
+                self.__getAllPossibleMovementToDirection(xChange, yChange, maxSteps, f)
+    
+    def __countAllPossibleMovementToDirection(self, x_change: int, y_change: int, max_steps=0):
+        '''Get all the positions that the piece can move to, including position that are being threatened by other pieces'''
+        if max_steps == 0:
+            max_steps = max(self.board.board_size_x, self.board.board_size_y)
+        count = 0
         for i in range(max_steps):
             new_pos = self.moveToDirection((i+1) * x_change, (i+1) * y_change)
             if (not self.board.isWithinBoard(new_pos[0], new_pos[1])):
                 break
             if self.board.isBlocked(new_pos[0], new_pos[1]):
-                # in the context of CSP, the piece violates constraints
-                # print("ERROR: CONSTRAINT VIOLATION", new_pos[0], new_pos[1])
-                steps.append(new_pos)
+                count += 1
                 break
-            steps.append(new_pos)
-        return steps
+            count += 1
+        return count
 
-    def getAllPossibleNewPos(self):
-        steps = []
+    def countAllPossibleNewPos(self):
+        count = 0
         for movement in self.movements:
             xChange, yChange, maxSteps = movement
-            steps.extend(self.__getAllPossibleMovementToDirection(xChange, yChange, maxSteps))
-        return steps
+            count += self.__countAllPossibleMovementToDirection(xChange, yChange, maxSteps)
+        return count
+
+    def __checkThreatenDirectional(self, x_change: int, y_change: int, max_steps=0) -> bool:
+        '''Get all the positions that the piece can move to, including position that are being threatened by other pieces'''
+        if max_steps == 0:
+            max_steps = max(self.board.board_size_x, self.board.board_size_y)
+        
+        for i in range(max_steps):
+            new_pos = self.moveToDirection((i+1) * x_change, (i+1) * y_change)
+            if (not self.board.isWithinBoard(new_pos[0], new_pos[1])):
+                break
+            if self.board.isBlocked(new_pos[0], new_pos[1]):
+                if self.board.isOccupiedByEnemyPiece(new_pos[0], new_pos[1]):
+                    return True
+                break
+        return False
+
+    def checkThreaten(self) -> bool:
+        for movement in self.movements:
+            xChange, yChange, maxSteps = movement
+            if self.__checkThreatenDirectional(xChange, yChange, maxSteps):
+                return True
+        return False
 
 class State:
 
@@ -142,7 +207,7 @@ class State:
         
     def setAssignment(self, assignment: dict):
         self.board = Board(self.cols, self.rows, self.listOfObstacles, self.maxNumberOfEachEnemies)
-        for pos in assignment.keys():
+        for pos in assignment:
             self.board.addEnemyPiece(assignment[pos], pos[0], pos[1])
     
     def updateAssignment(self, enemyType: str, position: tuple):
@@ -152,6 +217,12 @@ class State:
         # print(np.array([[len(x) for x in row] for row in self.board.possibleEnemyTypes]))
         remainingPiecesCount = sum([self.maxNumberOfEachEnemies[pieceType] for pieceType in Piece.enemyTypes]) - len(self.board.enemyPos)
         # print("remaining pieces", remainingPiecesCount)
+
+        # check if threatening each other
+        # for pos in self.board.enemyPos:
+        #     movements = pieceMovementModel(self.board, pos[0], pos[1], self.board.enemyPos[pos])
+        #     if movements.checkThreaten():
+        #         return False
 
         remainingPositionsCount = 0
         for x, y in list(itertools.product(range(self.cols), range(self.rows))):
@@ -206,6 +277,12 @@ def parser(testfile):
             "Rook": int(numOfEachEnemiesList[3]), 
             "Knight": int(numOfEachEnemiesList[4])
         } 
+    
+    # remove enemyType if is not in the csp
+    for enemyType in numOfEachEnemies:
+        if numOfEachEnemies[enemyType] == 0:
+            Piece.enemyTypes.remove(enemyType)
+    
     f.close()
 
     csp = State(rows, cols, listOfObstacles, numOfEachEnemies)
@@ -216,25 +293,16 @@ class Assignment:
     def __init__(self, maxNumOfEachEnemy) -> None:
         # numOfEachEnemies : King, Queen, Bishop, Rook, Knight
         self.maxNumOfEachEnemy = maxNumOfEachEnemy
-        self.currentNumOfEachEnemy = {
-            "King": 0, 
-            "Queen": 0, 
-            "Bishop": 0, 
-            "Rook": 0, 
-            "Knight": 0
-        } 
+        self.currentNumOfEachEnemy = {}
         self.assignment = {}
-        self.checked = {
-            "King": set(), 
-            "Queen": set(), 
-            "Bishop": set(), 
-            "Rook": set(), 
-            "Knight": set()
-        } 
+        self.checked = {}
+        for enemyType in Piece.enemyTypes:
+            self.currentNumOfEachEnemy[enemyType] = 0
+            self.checked[enemyType] = {}
 
     def copy(self):
         newCopy = Assignment(self.maxNumOfEachEnemy)
-        for position in self.assignment.keys():
+        for position in self.assignment:
             newCopy.addAssignment(self.assignment[position], position)
         for enemyType in Piece.enemyTypes:
             newCopy.checked[enemyType] = self.checked[enemyType].copy()
@@ -249,7 +317,7 @@ class Assignment:
     def addAssignment(self, enemyType: str, position: tuple):
         self.currentNumOfEachEnemy[enemyType] += 1
         self.assignment[position] = enemyType
-        self.checked[enemyType].add(position)
+        self.checked[enemyType][position] = True
     
     def removeAssignment(self, enemyType: str, position: tuple):
         self.currentNumOfEachEnemy[enemyType] -= 1
@@ -303,12 +371,11 @@ def orderDomainValues(csp: State, pieceType: str, assignment: Assignment):
             pieceType not in csp.board.possibleEnemyTypes[x][y]) or (
             (x, y) in assignment.checked[pieceType]):
             continue
-        result.append((x, y))
-        # transModel = pieceMovementModel(csp.board, x, y, pieceType)
-        # # add the number of positions threatened by the piece at that position to the list
-        # result.append((len(transModel.getAllPossibleNewPos()), (x, y)))
+        transModel = pieceMovementModel(csp.board, x, y, pieceType)
+        # add the number of positions threatened by the piece at that position to the list
+        result.append((transModel.countAllPossibleNewPos(), (x, y)))
 
-    # result.sort()
+    result.sort()
     return result
 
 def backTrack(csp: State, assignment: Assignment):
@@ -316,7 +383,7 @@ def backTrack(csp: State, assignment: Assignment):
     # input()
     # print("-"*80)
     enemyType = selectUnassignedVariable(csp, assignment)
-    for position in orderDomainValues(csp, enemyType, assignment):
+    for _, position in orderDomainValues(csp, enemyType, assignment):
         assignment.addAssignment(enemyType, position)
         csp.updateAssignment(enemyType, position)
         
@@ -327,12 +394,12 @@ def backTrack(csp: State, assignment: Assignment):
         inference = csp.inference()
         if inference != False:
             result = backTrack(csp, assignment.copy())
-            if result != "FAILURE":
+            if result != False:
                 return result
         assignment.removeAssignment(enemyType, position)
         csp.setAssignment(assignment.assignment)
         # print("Backtrack to", assignment.assignment)
-    return "FAILURE"
+    return False
 
 
 
@@ -354,11 +421,15 @@ def run_CSP():
     rawResult = search(testfile)
 
     goalState = {}
-    for pos in rawResult.keys():
+    for pos in rawResult:
         goalState[XYtoPos(pos)] = rawResult[pos]
     
     return goalState #Format to be returned
 
+# from time import time
 # startTime = time()
 # print(run_CSP())
 # print(time() - startTime)
+
+# import profile
+# profile.run("run_CSP()")
